f1519224b4dba46419a312d71db534f0f235d8be
DevUnanam <riaahh20@yahoo.com>
2024-11-22 16:33:52 -0800

    Removed double password hashing

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,11 +11,8 @@ exports.registerUser = async (req, res) => {
         const existingUser = await User.findOne({ email });
         if (existingUser) return res.status(400).json({ message: "User already exists" });
 
-        // Hash the password before saving
-        const hashedPassword = await bcrypt.hash(password, 10);
-
         // Create new user
-        const user = await User.create({ name, email, password: hashedPassword });
+        const user = await User.create({ name, email, password }); // Middleware will hash the password
 
         // Create JWT token
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
@@ -26,6 +23,7 @@ exports.registerUser = async (req, res) => {
         res.status(500).json({ message: "Server error", error: err.message });
     }
 };
+
 
 // Login User
 exports.loginUser = async (req, res) => {
